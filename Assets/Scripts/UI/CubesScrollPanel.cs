98a8eeb6ecc5cb3975a50a28da3505db4f0c42d7
Alexandr <rfst@mail.ru>
2025-10-21 21:28:18 +0700

    create CubesScrollPanel

---
+++ Assets/Scripts/UI/CubesScrollPanel.cs
@@ -0,0 +1,34 @@
+﻿using Core.Cubes;
+using Core.Cubes.Services;
+using Extensions;
+using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+    public class CubesScrollPanel : MonoBehaviour
+    {
+        [SerializeField] private CubeView cubePrefab;
+        [SerializeField] private RectTransform cubesRoot;
+
+        [Inject] private readonly IInstantiator _instantiator;
+        [Inject] private readonly CubesService _cubesService;
+
+        private void Start()
+        {
+            RefreshCubes();
+        }
+
+        private void RefreshCubes()
+        {
+            var cubesForSpawn = _cubesService.GetCubesForSpawn();
+            cubesRoot.ClearChildren();
+            
+            foreach (var cubeType in cubesForSpawn)
+            {
+                var cubeView = _instantiator.InstantiatePrefabForComponent<CubeView>(cubePrefab, cubesRoot);
+                cubeView.Setup(cubeType);
+            }
+        }
+    }
+}
