9d5f95e38730d2148b87499feee7499808bbd2bf
Alexandr <rfst@mail.ru>
2025-10-21 21:26:51 +0700

    create TransformExtensions

---
+++ Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class TransformExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ClearChildren(this Transform transform)
+        {
+#if UNITY_EDITOR
+            if (!UnityEditor.EditorApplication.isPlaying)
+            {
+                foreach (Transform child in transform)
+                {
+                    Object.DestroyImmediate(child.gameObject, false);
+                }
+
+                return;
+            }
+#endif
+
+            foreach (Transform child in transform)
+            {
+                Object.Destroy(child.gameObject);
+            }
+        }
+    }
+}
