7e281aaacd7e8e87f77c8bdb41b21660c68c9ed1
Alexandr <rfst@mail.ru>
2025-10-21 21:28:08 +0700

    create CubesService

---
+++ Assets/Scripts/Core/Cubes/Services/CubesService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Config;
+using Core.Cubes.Config;
+using Zenject;
+
+namespace Core.Cubes.Services
+{
+    public class CubesService
+    {
+        [Inject] private readonly ConfigData _configData;
+
+        public List<CubeType> GetCubesForSpawn()
+        {
+            var cubesShowCount = _configData.cubesConfig.cubesSpawnCount;
+            var cubeConfigList = _configData.cubesConfig.cubeConfigsList.Select(x => x.cubeType).ToList();
+            var cubesForSpawn = new List<CubeType>();
+            for (int i = 0; i < cubesShowCount; i++)
+            {
+                if (i >= cubeConfigList.Count)
+                {
+                    break;
+                }
+                
+                var cubeType = cubeConfigList[i];
+                cubesForSpawn.Add(cubeType);
+            }
+            
+            return cubesForSpawn;
+        }
+    }
+}
