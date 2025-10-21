3bcb44fe6e87576892ba8ebc0c5eb3892447f482
Alexandr <rfst@mail.ru>
2025-10-21 21:26:01 +0700

    create CubesSettings

---
+++ Assets/Scripts/Core/Cubes/Settings/CubesSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Cubes.Config;
+using UnityEngine;
+
+namespace Core.Cubes.Settings
+{
+    [CreateAssetMenu(fileName = "CubesSettings", menuName = "ScriptableObjects/CubesSettingsScriptableObject",
+        order = 1)]
+    public class CubesSettings : ScriptableObject
+    {
+        [Serializable]
+        private struct ConcreteCubeSettings
+        {
+            public CubeType cubeType;
+            public Sprite cubeSprite;
+        }
+
+        [SerializeField] private List<ConcreteCubeSettings> cubeSettings;
+
+        public Sprite GetCubeSprite(CubeType cubeType)
+        {
+            return GetCubeSettings(cubeType).cubeSprite;
+        }
+
+        private ConcreteCubeSettings GetCubeSettings(CubeType cubeType)
+        {
+            return cubeSettings.FirstOrDefault(x => x.cubeType == cubeType);
+        }
+    }
+}
