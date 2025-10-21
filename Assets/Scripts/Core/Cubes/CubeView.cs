316597dc13d079bef72ccdb703d8ae6d299e96e0
Alexandr <rfst@mail.ru>
2025-10-21 21:27:09 +0700

    create CubeView

---
+++ Assets/Scripts/Core/Cubes/CubeView.cs
@@ -0,0 +1,30 @@
+﻿using Core.Cubes.Config;
+using Core.Cubes.Settings;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Core.Cubes
+{
+    public class CubeView : MonoBehaviour
+    {
+        [SerializeField] private Image cubeBackground;
+
+        [Inject] private readonly CubesSettings _cubesSettings;
+
+        private CubeType _cubeType;
+        
+        public void Setup(CubeType cubeType)
+        {
+            _cubeType = cubeType;
+            
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            var cubeSprite = _cubesSettings.GetCubeSprite(_cubeType);
+            cubeBackground.sprite = cubeSprite;
+        }
+    }
+}
