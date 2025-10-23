8afd054ce7afa2685b673349f0bc7c949a390f4c
Alexandr <rfst@mail.ru>
2025-10-23 22:00:34 +0700

    create CubeActionSettings

--- Assets/Scripts/Installers/GameInstaller.cs
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -1,6 +1,7 @@
 using Core.Cubes;
 using Core.Cubes.Services;
 using Core.Cubes.Settings;
+using UI.Settings;
 using UnityEngine;
 using Zenject;
 
@@ -9,12 +10,14 @@ namespace Installers
     public class GameInstaller : MonoInstaller
     {
         [SerializeField] private CubesSettings cubesSettings;
+        [SerializeField] private CubeActionSettings cubesActionSettings;
         [SerializeField] private CubesFactory cubesFactory;
 
         public override void InstallBindings()
         {
             Container.Bind<CubesService>().AsSingle();
             Container.BindInstance(cubesSettings).AsSingle();
+            Container.BindInstance(cubesActionSettings).AsSingle();
             Container.BindInstance(cubesFactory).AsSingle();
         }
     }
---
+++ Assets/Scripts/UI/Settings/CubeActionSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UI.Settings
+{
+    [CreateAssetMenu(fileName = "CubeActionSettings", menuName = "ScriptableObjects/CubeActionSettingsScriptableObject",
+        order = 1)]
+    public class CubeActionSettings : ScriptableObject
+    {
+        [Serializable]
+        private struct ConcreteCubeActionSettings
+        {
+            public CubeActionType actionType;
+            public string actionText;
+        }
+        
+        [SerializeField] private List<ConcreteCubeActionSettings> cubeActionSettings;
+
+        public string GetActionText(CubeActionType actionType)
+        {
+            return GetCubeActionSettings(actionType).actionText;
+        }
+
+        private ConcreteCubeActionSettings GetCubeActionSettings(CubeActionType actionType)
+        {
+            return cubeActionSettings.First(x => x.actionType == actionType);
+        }
+    }
+}
