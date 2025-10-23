d640fbd85c65c1072190fe68ba50bab0d3971558
Alexandr <rfst@mail.ru>
2025-10-23 17:37:02 +0700

    update CubesService

--- Assets/Scripts/Core/Cubes/Services/CubesService.cs
+++ Assets/Scripts/Core/Cubes/Services/CubesService.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Config;
 using Core.Cubes.Config;
+using Data;
+using UnityEngine;
 using Zenject;
 
 namespace Core.Cubes.Services
@@ -9,6 +11,9 @@ namespace Core.Cubes.Services
     public class CubesService
     {
         [Inject] private readonly ConfigData _configData;
+        [Inject] private readonly PlayerDataContainer _playerDataContainer;
+
+        private CubeTowerController _towerController;
 
         public List<CubeType> GetCubesForSpawn()
         {
@@ -21,12 +26,48 @@ namespace Core.Cubes.Services
                 {
                     break;
                 }
-                
+
                 var cubeType = cubeConfigList[i];
                 cubesForSpawn.Add(cubeType);
             }
-            
+
             return cubesForSpawn;
+        }
+
+        public void RegisterCubeTower(CubeTowerController towerController)
+        {
+            _towerController = towerController;
+        }
+
+        public bool CubeCanPlaceAtTower(Vector2 screenPos)
+        {
+            if (_towerController == null)
+            {
+                return false;
+            }
+
+            return _towerController.CanPlaceAtTower(screenPos);
+        }
+
+        public void TryPlaceAtTowerScreen(CubeType cubeType, Vector2 screenPos)
+        {
+            if (_towerController == null)
+            {
+                return;
+            }
+
+            if (!RectTransformUtility.RectangleContainsScreenPoint(_towerController.TowerAreaRect, screenPos,
+                    _towerController.Canvas.worldCamera))
+            {
+                return;
+            }
+
+            _towerController.PlaceCube(cubeType, screenPos);
+        }
+
+        public float GetCubeSpawnHorizontalOffset()
+        {
+            return _configData.cubesConfig.spawnOffsetPercent;
         }
     }
 }
